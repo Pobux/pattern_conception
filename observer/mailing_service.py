7e64624627d40e0fd526473589c69e774e3f6031
Antoine L <antoine.lebel.65@gmail.com>
2016-10-21 20:39:20 -0400

    beautitfy observer

--- observer/mailing_service.py
+++ observer/mailing_service.py
@@ -5,10 +5,14 @@ import observer
 
 class MailingService(observer.Observer):
     MIN_HUMIDITY = 20
+    def __init__(self):
+        self.sensor = None
 
     def update(self, HumiditySensor):
-        if HumiditySensor.humidity < self.MIN_HUMIDITY:
-            self._send_humidity_warning(HumiditySensor.humidity)
+        self.sensor = HumiditySensor
 
-    def _send_humidity_warning(self, humidity):
+        if HumiditySensor.humidity < self.MIN_HUMIDITY:
+            self._send_humidity_warning()
+
+    def _send_humidity_warning(self):
         print("Un message courriel a été envoyé pour avertir que les plantes sont sèches!")
--- observer/website_notifier.py
+++ observer/website_notifier.py
@@ -5,13 +5,17 @@ import observer
 
 class WebsiteNotifier(observer.Observer):
 
+    def __init__(self):
+        self.sensor = None
+
     def update(self, HumidititySensor):
+        self.sensor = HumidititySensor
         self._connect()
-        self._update_humidity(HumidititySensor.humidity)
+        self._update_humidity()
 
     def _connect(self):
         #connection à la base de données du siteweb
         pass
 
-    def _update_humidity(self, humidity):
-        print("Le siteweb a été mis à jour.")
+    def _update_humidity(self):
+        print("Le siteweb a été mis à jour " + str(self.sensor.humidity) + ".")
